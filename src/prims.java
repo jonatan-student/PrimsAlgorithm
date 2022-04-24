4ad039a138dcf77b2eaae34840b7c1c77918a9d0
jonatan-student <78746701+jonatan-student@users.noreply.github.com>
2022-04-24 13:40:39 +0200

    done did an organize

---
+++ src/prims.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+public class prims {
+    ArrayList<Vertex> MST;
+    ArrayList<Edge> NeededEdges;
+    Vertex Initial;
+    Integer TotalWeight;
+
+    public prims(){}
+
+}
